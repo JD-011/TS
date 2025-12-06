69432c467e051db3c9bc5016f546051bbc49bf89
JD-011 <jayveerdahiya011@gmail.com>
2025-12-06 21:58:33 +0530

    Type Annotations and Inference in TypeScript

---
+++ src/typesInTS.ts
@@ -0,0 +1,13 @@
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : "5"
+
+let channleName = "chaiaurcode"
+// channleName = 106 // Error: Type 'number' is not assignable to type 'string'.
+
+let chaiFlavor: string = "masala"
+chaiFlavor = "ginger"
+
+let chaiOrder: number = 1
+
+let isHot: boolean = true

ca31e473d3cb3083d9eef471aab2d52e50b693a1
JD-011 <jayveerdahiya011@gmail.com>
2025-12-17 09:50:01 +0530

    axios in TypeScript

---
+++ src/webReq.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import type { AxiosResponse } from 'axios';
+
+interface Todo {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
+
+const fetchTodo = async() => {
+    try {
+        const res: AxiosResponse<Todo> = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        console.log("Todo:", res.data)
+    } catch (err: any) {
+        if(axios.isAxiosError(err)){
+            console.error("Axios Error:", err.message)
+            if(err.response){
+                console.error("Error code:", err.response.status)
+            }
+        }
+        console.error("Error fetching Todo:", err)
+    }
+}
+
+fetchTodo()
